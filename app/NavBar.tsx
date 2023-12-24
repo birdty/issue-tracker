c35b1a98951166e70119259e9a187713c732c51a
Tyler Bird <birdty@epromo.com>
2023-12-23 20:38:24 -0700

    o - before using classnames library

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,16 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
+  const currentPath = usePathname();
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -18,7 +22,9 @@ const NavBar = () => {
           <Link
             href={link.href}
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={`${
+              currentPath == link.href ? "text-zine-900" : "text-zinc-500"
+            } hover:text-zinc-800 transition-colors`}
           >
             {link.label}
           </Link>
